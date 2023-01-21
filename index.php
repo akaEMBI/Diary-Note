cd46d769be1aa82b0ba02319d633229a97c3ecb9
Surya Astawan <95583159+akaEMBI@users.noreply.github.com>
2023-01-21 19:45:13 +0800

    Menambahkan Register Form dan Forgot Password Form pada index.php | memperbaharui style.css

--- index.php
+++ index.php
@@ -87,6 +87,124 @@
 				</div>
 			</div>
 			<!-- Login Form End -->
+			<!-- Register Form Start -->
+			<div class="row justify-content-center wrapper" id="register-box" style="display: none">
+				<div class="col-lg-10 my-auto">
+					<div class="card-group myShadow">
+						<div class="card justify-content-center rounded-right myColor p-4">
+							<h1 class="text-center font-weight-bold text-white">Welcome Back!</h1>
+							<hr class="my-3 bg-light myHr" />
+							<p class="text-center font-weight-bolder text-light lead">
+								To keep connected with us please login with your personal info.
+							</p>
+							<button class="btn btn-outline-light btn-lg align-self-center font-weight-bolder mt-4 myLinkBtn" id="login-link">
+								Sign In
+							</button>
+						</div>
+						<div class="card rounded-left p-4" style="flex-grow: 1.4">
+							<h1 class="text-center font-weight-bold text-primary">Create Account</h1>
+							<hr class="my-3" />
+							<form action="#" method="post" class="px-3" id="register-form">
+								<div class="input-group input-group-lg form-group">
+									<div class="input-group-prepend">
+										<span class="input-group-text rounded-0">
+											<i class="far fa-user fa-lg"></i>
+										</span>
+									</div>
+									<input type="text" name="name" id="name" class="form-control rounded-0" placeholder="Username" required />
+								</div>
+								<!-- email-form-start -->
+								<div class="input-group input-group-lg form-group">
+									<div class="input-group-prepend">
+										<span class="input-group-text rounded-0">
+											<i class="far fa-envelope fa-lg"></i>
+										</span>
+									</div>
+									<input type="email" name="email" id="remail" class="form-control rounded-0" placeholder="E-Mail" required />
+								</div>
+								<!-- email-form-end -->
+								<!-- password-form-start -->
+								<div class="input-group input-group-lg form-group">
+									<div class="input-group-prepend">
+										<span class="input-group-text rounded-0">
+											<i class="fas fa-key fa-lg"></i>
+										</span>
+									</div>
+									<input
+										type="password"
+										name="password"
+										id="rpassword"
+										class="form-control rounded-0"
+										placeholder="Password"
+										required
+										minlength="5"
+									/>
+								</div>
+								<!-- password-form-end -->
+								<div class="input-group input-group-lg form-group">
+									<div class="input-group-prepend">
+										<span class="input-group-text rounded-0">
+											<i class="fas fa-key fa-lg"></i>
+										</span>
+									</div>
+									<input
+										type="password"
+										name="cpassword"
+										id="cpassword"
+										class="form-control rounded-0"
+										placeholder="Confirm Password"
+										required
+										minlength="5"
+									/>
+								</div>
+								<div class="form-group">
+									<input type="submit" value="Sign Up" id="register-btn" class="btn btn-primary btn-lg btn-block myBtn" />
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Register Form End -->
+
+			<!-- Forgot Password Form Start -->
+			<div class="row justify-content-center wrapper" id="forgot-box" style="display: none">
+				<div class="col-lg-10 my-auto">
+					<div class="card-group myShadow">
+						<div class="card justify-content-center rounded-left myColor p-4">
+							<h1 class="text-center font-weight-bold text-white">Reset Password</h1>
+							<hr class="my-3 bg-light myHr" />
+							<button class="btn btn-outline-light btn-lg align-self-center font-weight-bolder mt-4 myLinkBtn" id="back-link">
+								Back
+							</button>
+						</div>
+						<div class="card rounded-right p-4" style="flex-grow: 1.4">
+							<h1 class="text-center font-weight-bold text-primary">Forgot Your Password</h1>
+							<hr class="my-3" />
+							<p class="lead text-center text-secondary">
+								To reset your password, enter the registered e-mail address and we will send you the reset instructions on your
+								e-mail!
+							</p>
+							<form action="#" method="post" class="px-3" id="forgot-form">
+								<!-- email-form-start -->
+								<div class="input-group input-group-lg form-group">
+									<div class="input-group-prepend">
+										<span class="input-group-text rounded-0">
+											<i class="far fa-envelope fa-lg"></i>
+										</span>
+									</div>
+									<input type="email" name="email" id="femail" class="form-control rounded-0" placeholder="E-Mail" required />
+								</div>
+								<!-- email-form-end -->
+								<div class="form-group">
+									<input type="submit" value="Reset Password" id="forgot-btn" class="btn btn-primary btn-lg btn-block myBtn" />
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Forgot Password Form End -->
 		</div>
 
 		<script
@@ -107,5 +225,25 @@
 			crossorigin="anonymous"
 			referrerpolicy="no-referrer"
 		></script>
+		<script type="text/javascript">
+			$(document).ready(function () {
+				$('#register-link').click(function () {
+					$('#login-box').hide();
+					$('#register-box').show();
+				});
+				$('#login-link').click(function () {
+					$('#login-box').show();
+					$('#register-box').hide();
+				});
+				$('#forgot-link').click(function () {
+					$('#login-box').hide();
+					$('#forgot-box').show();
+				});
+				$('#back-link').click(function () {
+					$('#login-box').show();
+					$('#forgot-box').hide();
+				});
+			});
+		</script>
 	</body>
 </html>
